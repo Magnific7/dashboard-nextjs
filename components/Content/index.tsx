63d4811f3710ca632cba97240c2292fc77cf2cf5
Magnific7 <mjones6944@gmail.com>
2022-04-30 17:59:07 +0200

    so far not bad

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -1,55 +1,120 @@
-import React from "react";
+import React, { FC } from "react";
 import styles from "./index.module.scss";
 import "chart.js/auto";
-import { Bubble, Line, Doughnut } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+} from "recharts";
 
-const Content = () => {
-  const data = {
-    labels: [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ],
-    datasets: [
-      {
-        label: "Sales/ month",
-        fill: true,
-        lineTension: 0.1,
-        backgroundColor: "rgba(75,192,192,0.4)",
-        borderColor: "rgba(75,192,192,1)",
-        borderCapStyle: "butt",
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: "miter",
-        pointBorderColor: "rgba(75,192,192,1)",
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 0,
-        pointHoverRadius: 0,
-        pointHoverBackgroundColor: "rgba(75,192,192,1)",
-        pointHoverBorderColor: "rgba(220,220,220,1)",
-        pointHoverBorderWidth: 0,
-        pointRadius: 0,
-        pointHitRadius: 0,
-        data: [65, 59, 80, 81, 56, 55, 40, 57, 40, 48, 59, 62],
+export interface ContentProps {
+  children?: JSX.Element | JSX.Element[];
+}
+
+const Content: FC<ContentProps> = (children, li) => {
+  const data = [
+    {
+      name: "Jan",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Feb",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Mar",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "April",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "May",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "June",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "July",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+    {
+      name: "Aug",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Sept",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Oct",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Nov",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Dec",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+  ];
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
       },
-    ],
+    },
   };
   const data1 = {
-    labels: ["Organic", "Social Media", "Websites"],
+    labels: ["Organic", "Social Media", "Websites", "sensation"],
     datasets: [
       {
-        data: [300, 50, 100],
-        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-        hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        data: [500.0, 50.0, 100.0, 200.0],
+        backgroundColor: [
+          "rgba(75,192,192,0.4)",
+          "rgba(75,192,192,1)",
+          "rgba(220,220,220,1)",
+          "rgb(20, 99, 89)",
+        ],
+        hoverBackgroundColor: [
+          "rgba(75,192,192,0.4)",
+          "rgba(75,192,192,1)",
+          "rgba(220,220,220,1)",
+          "rgb(20, 99, 89)",
+        ],
       },
     ],
   };
@@ -58,35 +123,161 @@ const Content = () => {
     <div className={styles.Content}>
       <div className={styles.Content__wrapper}>
         <div className={styles.Content__wrapper__tabs}>
-          <div className={styles.Content__wrapper__tabs__categories}>
-            <h2>Companies</h2>
+          <div className={styles.Content__wrapper__tabs__left}>
+            <h2>chash flow</h2>
+            <p>Past 12 months</p>
+            <h1>$43,100</h1>
+            <p>current cash balance</p>
           </div>
-        </div>
-        <div className={styles.Content__wrapper__tabs}>
-          <div className={styles.Content__wrapper__tabs__categories}>
-            <h2>Customers</h2>
-          </div>
-        </div>{" "}
-        <div className={styles.Content__wrapper__tabs}>
-          <div className={styles.Content__wrapper__tabs__categories}>
-            <h2>Users</h2>
-          </div>
-        </div>{" "}
-        <div className={styles.Content__wrapper__tabs}>
-          <div className={styles.Content__wrapper__tabs__categories}>
-            <h2>Project</h2>
+          <div className={styles.Content__wrapper__tabs__right}>
+            <div className={styles.Content__wrapper__tabs__right__in}>
+              <div className={styles.Content__wrapper__tabs__right__in__one}>
+                <span>Money In/out</span>
+              </div>
+              <div className={styles.Content__wrapper__tabs__right__in__two}>
+                <span>Bank Balance</span>
+              </div>
+            </div>
+            <div className={styles.Content__wrapper__tabs__right__dot}>
+              <div
+                className={styles.Content__wrapper__tabs__right__dot__in}
+              ></div>
+              <h3>Money In</h3>
+              <div
+                className={styles.Content__wrapper__tabs__right__dot__out}
+              ></div>
+              <h3>Money Out</h3>
+            </div>
           </div>
         </div>
       </div>
 
       <div className={styles.charts}>
         <div className={styles.charts__bar}>
-          <h2>Sales</h2>
-          <Line data={data} width={400} height={400} />
+          <div className={styles.charts__bar__graph}>
+            <BarChart
+              width={1000}
+              height={250}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="pv" fill="rgb(143, 212, 175)" />
+              <Bar dataKey="uv" fill="rgb(200, 243, 220)" />
+            </BarChart>{" "}
+          </div>
         </div>
-        <div className={styles.charts__circle}>
-          <h2>Customers arrived </h2>
-          <Doughnut data={data1} width={400} height={400} />
+        <div className={styles.charts__sales}>
+          <div className={styles.charts__sales__expenses}>
+            <h3>Invoces</h3>
+            <p>
+              <b>$3,500.00 Unpaid</b> Last 365 Days
+            </p>
+            <div className={styles.charts__sales__expenses__amount}>
+              <div className={styles.charts__sales__expenses__amount__left}>
+                <h1>$3,500.00</h1>
+                <h4>Overdue</h4>
+              </div>
+              <div className={styles.charts__sales__expenses__amount__right}>
+                <h1>$3,500.00</h1>
+                <h4>Overdue</h4>
+              </div>
+            </div>
+            <div className={styles.charts__sales__expenses__progress}>
+              <div
+                className={styles.charts__sales__expenses__progress__bar}
+              ></div>
+            </div>
+            <p>
+              <b>$3,500.00 Unpaid</b> Last 365 Days
+            </p>
+            <div className={styles.charts__sales__expenses__amount}>
+              <div className={styles.charts__sales__expenses__amount__left}>
+                <h1>$3,500.00</h1>
+                <h4>Overdue</h4>
+              </div>
+              <div className={styles.charts__sales__expenses__amount__right}>
+                <h1>$3,500.00</h1>
+                <h4>Overdue</h4>
+              </div>
+            </div>
+          </div>
+          <div className={styles.charts__sales__invoices}>
+            <div className={styles.charts__sales__invoices__right}>
+              <h3>Expenses</h3>
+              <h1>$3,500.00</h1>
+              <h4>last Month</h4>
+              <h4></h4>
+              <div className={styles.charts__sales__invoices__right__doughbut}>
+                <Doughnut
+                  data={data1}
+                  width={300}
+                  height={300}
+                  options={options}
+                />
+              </div>
+            </div>
+
+            <div className={styles.charts__sales__invoices__leftside}>
+              <div className={styles.charts__sales__invoices__leftside__data}>
+                <div
+                  className={
+                    styles.charts__sales__invoices__leftside__data__dot
+                  }
+                  style={{
+                    backgroundColor: `${data1.datasets[0].backgroundColor[0]}`,
+                  }}
+                ></div>
+                <h2>${data1.datasets[0].data[0]}</h2>
+                <p>{data1.labels[0]}</p>
+              </div>
+              <div className={styles.charts__sales__invoices__leftside__data}>
+                <div
+                  className={
+                    styles.charts__sales__invoices__leftside__data__dot
+                  }
+                  style={{
+                    backgroundColor: `${data1.datasets[0].backgroundColor[1]}`,
+                  }}
+                ></div>
+                <h2>${data1.datasets[0].data[1]}</h2>
+                <p>{data1.labels[1]}</p>
+              </div>{" "}
+              <div className={styles.charts__sales__invoices__leftside__data}>
+                <div
+                  className={
+                    styles.charts__sales__invoices__leftside__data__dot
+                  }
+                  style={{
+                    backgroundColor: `${data1.datasets[0].backgroundColor[2]}`,
+                  }}
+                ></div>
+                <h2>${data1.datasets[0].data[2]}</h2>
+                <p>{data1.labels[2]}</p>
+              </div>{" "}
+              <div className={styles.charts__sales__invoices__leftside__data}>
+                <div
+                  className={
+                    styles.charts__sales__invoices__leftside__data__dot
+                  }
+                  style={{
+                    backgroundColor: `${data1.datasets[0].backgroundColor[3]}`,
+                  }}
+                ></div>
+                <h2>${data1.datasets[0].data[3]}</h2>
+                <p>{data1.labels[3]}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,22 +2,32 @@ import React from "react";
 import styles from "./index.module.scss";
 import Image from "next/image";
 import image from "../../public/watermark.png";
+import { Input } from "semantic-ui-react";
+import logo from "./../../public/orrchi.png";
 
 const Header = () => {
   return (
     <div className={styles.Header}>
       <div className={styles.Header__wrapper}>
         <div className={styles.Header__wrapper__title}>
-          <h2>Hello, Mary</h2>
-          <p>Welcome to the dashboard </p>
+          <Image
+            src={logo}
+            alt="logo"
+            className={styles.Header__wrapper__title__logo}
+          />
         </div>
-      </div>
-      <div className={styles.Header__profile}>
-        <Image
-          src={image}
-          alt="profile"
-          className={styles.Header__profile_image}
-        />
+        <div className={styles.Header__wrapper__right}>
+          <div className={styles.Header__wrapper__right__search}>
+            <Input icon="search green" placeholder="Search ..." />
+          </div>
+          <div className={styles.Header__wrapper__right__profile}>
+            <Image
+              src={image}
+              alt="profile"
+              className={styles.Header__wrapper__right__profile_image}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
