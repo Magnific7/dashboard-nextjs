321e86dc045c02a2a2f3d72b385a6c99cd3d8e1a
Magnific7 <mjones6944@gmail.com>
2022-05-04 12:02:32 +0200

    feat make bar chart responsive

--- components/charts/BarComponent/index.jsx
+++ components/charts/BarComponent/index.jsx
@@ -89,28 +89,26 @@ const BarComponent = (children, li) => {
   // console.log("SCRN WIDRH", screen.width);
   return (
     <div className={styles.bar_container}>
-      <div className={styles.bar_container}>
-        <div className={styles.bar_container__graph}>
-          <BarChart
-            width={1000}
-            height={250}
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="pv" fill="rgb(83, 182, 131)" />
-            <Bar dataKey="uv" fill="rgb(202, 238, 240)" />
-          </BarChart>
-        </div>
+      <div className={styles.bar_container__graph}>
+        <BarChart
+          width={1000}
+          height={250}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="rgb(83, 182, 131)" />
+          <Bar dataKey="uv" fill="rgb(202, 238, 240)" />
+        </BarChart>
       </div>
     </div>
   );
